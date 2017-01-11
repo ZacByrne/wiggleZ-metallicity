5110daf2591ee373ee7cecc26120784cf90c2c75
ZacByrne <zacbyrne@gmail.com>
2017-01-11 14:35:06 +1000

    Add files via upload

--- metal/metal.py
+++ metal/metal.py
@@ -17,7 +17,7 @@ def adjustflux(flux, kred):
 #load values
 #import args from system? (Google)
 # import line flux values from file into matrix.. NEED TO SEE OUTPUT FILE... CHANGE TO USECOL()
-col, oiiiw, oiiiwe, oii, oiie, oiii, oiiie, hb, hbe, hg, hge = np.loadtxt("fluxes.txt", dtype = {'names': ('column name', '4363', '4363 error ', '3728', '3278 error', '4959', '4959 error', 'hbeta', 'hbeta error'), 'formats': ('S4', 'f8','f8','f8','f8','f8','f8','f8','f8')}, skiprows=1)
+col, oiiiw, oiiiwe, oii, oiie, oiii, oiiie, hb, hbe, hg, hge = np.loadtxt("fluxes.txt", dtype = {'names': ('column name', '4363', '4363 error ', '3728', '3278 error', '4959', '4959 error', 'hbeta', 'hbeta error', 'hgamma', 'hgamma error'), 'formats': ('S4', 'f8','f8','f8','f8','f8','f8','f8','f8','f8','f8')}, skiprows=1, unpack=True)
 
 
 # calculate k values? Or simply import from matlab program?
@@ -41,36 +41,36 @@ summet2 = 0
 for x in range(0,count):
     
     #Uncertainity calculations:
-    oiiiw = oiiiw + np.random.normal(size = (len(oiiiw))) * oiiiwe
-    oiii = oiii + np.random.normal(size = (len(oiiiw))) * oiiie
-    oii = oii + np.random.normal(size = (len(oiiiw))) * oiie
-    hb = hb + np.random.normal(size = (len(oiiiw))) * hbe
+    oiiiwu = oiiiw + np.random.normal(size = (len(oiiiw))) * oiiiwe
+    oiiiu = oiii + np.random.normal(size = (len(oiiiw))) * oiiie
+    oiiu = oii + np.random.normal(size = (len(oiiiw))) * oiie
+    hbu = hb + np.random.normal(size = (len(oiiiw))) * hbe
     
     # Adjust line fluxes for reddening as per Ly 2016
-    oiiiw = adjustflux(oiiiw, k4363)
-    oii = adjustflux(oii, k3728)
-    oiii = adjustflux(oiii, k4959)
-    hb = adjustflux(hb, k4861)
+    oiiiwa = adjustflux(oiiiwu, k4363)
+    oiia = adjustflux(oiiu, k3728)
+    oiiia = adjustflux(oiiiu, k4959)
+    hba = adjustflux(hbu, k4861)
 
     # calc OIII temp as per Nichollis et al. 2014 
-    oiiitemp = 13205 * (-1*math.log10((oiiiw / (oiii*4)))-0.92506)**(-0.98062)
+    oiiitemp = 13205 * (-1*np.log10((oiiiwa / (oiiia*4)))-0.92506)**(-0.98062)
 
     #Calc OII temp as per Andrews and Martini 2013
     oiitemp = 0.7 * oiiitemp + 0.17 * 10000
 
     #Calculate OII and OIII metallicity
-    oiimet = math.log10(oii/hb)+5.961+1.676/oiitemp - 0.4*math.log10(oiitemp)-0.034*oiitemp
-    oiiimet = math.log10(4*oiii/hb)+6.2+1.251/oiiitemp - 0.55*math.log10(oiitemp)-0.014*oiiitemp
+    oiimet = np.log10(oiia/hba)+5.961+1.676/oiitemp - 0.4*np.log10(oiitemp)-0.034*oiitemp
+    oiiimet = np.log10(4*oiiia/hba)+6.2+1.251/oiiitemp - 0.55*np.log10(oiitemp)-0.014*oiiitemp
 
     #Calculate O+ and O++ metallicity
-    metal = math.log10(10**(oiimet-12)+10**(oiiimet-12))+12
-    
+    metal = np.log10(10**(oiimet-12)+10**(oiiimet-12))+12
+    print metal
     summet += metal
     summet += metal**2
     
 #end of loop thing   
 summet = summet / count
-summet2 = math.sqrt(summet2/ count - (summet)**2)
+summet2 = np.sqrt(summet2/ count - (summet)**2)
 
 print summet
 print summet2
