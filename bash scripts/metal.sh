9c253e96b40876084bbbcc7e1f26e9d65a4f821a
ZacByrne <zacbyrne@gmail.com>
2017-03-01 15:48:24 +1000

    Update metal.sh

--- bash scripts/metal.sh
+++ bash scripts/metal.sh
@@ -1,8 +1,25 @@
 #!/usr/bin/env python
+#!/bin/bash
 
 # Run make file for bins?
+massbin = $1
+redbin = $2
+
+sh ./wig02make.txt $massbin $redbin
+
 # Run Starlgiht
+
+
 # turn starlight files into one fitted spectrum
+for i in {0..massbin}
+do
+for j in {0..redbin}
+do
+python create2gauss.py $i$j
+done
+done
+
+ls wig02fitted*.txt > list.txt
 
 #run twogauss
 ./twogauss < tginput
